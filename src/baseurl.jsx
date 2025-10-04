47ff7d354f1aadcf24570062a5a0d9e0de7ee948
DawrAli33285 <lemightyeagle@gmail.com>
2025-10-04 07:11:08 -0700

    new

--- src/baseurl.jsx
+++ src/baseurl.jsx
@@ -1,2 +1,4 @@
-export const BASE_URL="http://localhost:5000"
-export const FRONTEND_URL="http://localhost:3000"
+// export const BASE_URL="http://localhost:5000"
+export const BASE_URL="https://newreviewbackend.vercel.app"
+// export const FRONTEND_URL="http://localhost:3000"
+export const FRONTEND_URL="https://newreviewwebsitefrontend.vercel.app"
