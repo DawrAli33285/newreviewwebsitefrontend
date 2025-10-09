817322e473cd22bfb383ec72cf0432a49b17a32c
DawrAli33285 <lemightyeagle@gmail.com>
2025-10-09 13:43:58 -0700

    new

--- src/baseurl.jsx
+++ src/baseurl.jsx
@@ -1,4 +1,8 @@
 
-export const BASE_URL="https://newreviewbackend.vercel.app"
+// export const BASE_URL="https://newreviewbackend.vercel.app"
+export const BASE_URL="http://38.242.226.85:5000"
 
-export const FRONTEND_URL="https://newreviewwebsitefrontend.vercel.app"
+
+// export const FRONTEND_URL="https://newreviewwebsitefrontend.vercel.app"
+
+export const FRONTEND_URL="https://rate-our-restaurant.com"
